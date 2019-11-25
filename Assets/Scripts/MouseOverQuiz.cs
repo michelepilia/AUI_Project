042687c0408f56daf972620172928a8a2454614a
michelepilia <michele.pilia@outlook.it>
2019-11-25 18:27:37 +0100

    Quiz fatto

---
+++ Assets/Scripts/MouseOverQuiz.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverQuiz : MonoBehaviour
+{
+    
+
+    public bool isCorrect;
+    public Material correctMaterial;
+    public Material wrongMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (isCorrect)
+        {
+            GetComponent<Renderer>().material = correctMaterial;
+        }
+        else
+        {
+            GetComponent<Renderer>().material = wrongMaterial;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/clickOpenQuiz.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickOpenQuiz : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject.Find("GeneralCanvas").SetActive(false);
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(true);
+        transform.GetChild(2).gameObject.SetActive(true);
+        transform.GetChild(3).gameObject.SetActive(true);
+        Quiz();
+    }
+
+    private void Quiz() {
+
+    }
+
+}
