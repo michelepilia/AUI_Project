22a2ec93b58aa5dd5555a71146729d3f4fcb14c2
Antonino96 <10546001@polimi.it>
2019-11-25 18:03:08 +0100

    prima bozza

---
+++ Assets/Scripts/MovingBackLight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackLight : MonoBehaviour
+{
+    public Light myLight;
+    float startTime;
+    void Start()
+    {
+        GetComponent<Light>();
+        startTime = Time.time;
+        Invoke("m1", 4.0f);
+    }
+
+    void m1() {
+        startTime = Time.time;
+        Invoke("m1",4.0f);
+    }
+
+    void Update() {
+        if (Time.time - startTime < 2)
+        {
+            transform.Translate(Vector3.down*Time.deltaTime);
+        }
+        else if(Time.time - startTime >= 2 && Time.time- startTime<4)
+        {
+            transform.Translate(Vector3.up*Time.deltaTime);
+        }
+        
+
+    }
+}
