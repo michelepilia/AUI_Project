1f308528cf42bbff1b0ef50d8154e8e1a58d2758
Antonino96 <10546001@polimi.it>
2019-11-25 17:15:26 +0100

    added first quick scene

---
+++ Assets/Scripts/ChangeInstrument.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeInstrument : MonoBehaviour
+{
+    public Material full;
+    public Material hammond;
+    public Material bass;
+    public Material drums;
+    public Material guitar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material = full;
+        Invoke("m2", 2.0f);
+    }
+
+    void m1() {
+        GetComponent<Renderer>().material = full;
+        Invoke("m2",1.0f);
+    }
+    void m2() {
+        GetComponent<Renderer>().material = bass;
+        Invoke("m3",1.0f);
+    }
+    void m3() {
+        GetComponent<Renderer>().material = drums;
+        Invoke("m4",1.0f);
+    }
+    void m4() {
+        GetComponent<Renderer>().material = hammond;
+        Invoke("m5",1.0f);
+    }
+    void m5() {
+        GetComponent<Renderer>().material = guitar;
+        Invoke("m1",1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LightPinSpot.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPinSpot : MonoBehaviour
+{
+  
+    public Light myLight;
+    float startTime;
+    //float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //pause on guitar
+        if (Time.time - startTime < 2)
+        {
+            transform.Translate(Vector3.zero *Time.deltaTime); 
+        }
+        //moving on hammond 1
+        else if (Time.time - startTime >= 2 && Time.time - startTime < 5 )
+        {
+            transform.Translate(Vector3.left*Time.deltaTime); 
+        }
+        //moving on hammond 2
+        else if (Time.time - startTime >= 5 && Time.time - startTime < 6 )
+        {
+            transform.Translate(Vector3.up*Time.deltaTime); 
+        }
+        //pause on hammond
+        else if (Time.time - startTime >= 6 && Time.time - startTime < 8 )
+        {
+            transform.Translate(Vector3.zero*Time.deltaTime); 
+        }
+        //moving on drums 1
+        else if (Time.time - startTime >= 8 && Time.time - startTime < 13 )
+        {
+            transform.Translate(Vector3.right*Time.deltaTime); 
+        }
+        //moving on drums 2
+        else if (Time.time - startTime >= 13 && Time.time - startTime < 15 )
+        {
+            transform.Translate(Vector3.up*Time.deltaTime); 
+        }
+        //pause on drums
+        else if (Time.time - startTime >= 15 && Time.time - startTime < 17)
+        {
+            transform.Translate(Vector3.zero*Time.deltaTime); 
+        }
+        //Moving to bass 1
+        else if (Time.time - startTime >= 17 && Time.time - startTime < 19 )
+        {
+            transform.Translate(Vector3.down*Time.deltaTime); 
+        }
+        //Moving to bass 2
+        else if (Time.time - startTime >= 19 && Time.time - startTime < 20.5 )
+        {
+            transform.Translate(Vector3.right*Time.deltaTime); 
+        }
+        //pause on bass
+        else if (Time.time - startTime >= 20.5)
+        {
+            transform.Translate(Vector3.zero*Time.deltaTime); 
+        }
+    }
+
+}
