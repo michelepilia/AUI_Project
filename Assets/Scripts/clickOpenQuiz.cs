b9db72b30053f4fdb5b91cf89dfe12947739c02e
michelepilia <michele.pilia@outlook.it>
2019-11-26 02:35:50 +0100

    Aggiunto bottone indietro

--- Assets/Scripts/clickOpenQuiz.cs
+++ Assets/Scripts/clickOpenQuiz.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class clickOpenQuiz : MonoBehaviour
 {
+    public GameObject wallCamera;
+    public GameObject genCanvas;
+    public GameObject[] bandArray = new GameObject[4];
+    public Button backButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Button btn = backButton.GetComponent<Button>();
+        btn.onClick.AddListener(OnClickBackButton);
     }
 
     // Update is called once per frame
@@ -20,11 +28,26 @@ public class clickOpenQuiz : MonoBehaviour
     {
         GameObject.Find("Wall Camera").SetActive(false);
         GameObject.Find("GeneralCanvas").SetActive(false);
-        //GameObject.Find("BackButtonCanvas").SetActive(true);
+        backButton.gameObject.SetActive(true);
         transform.GetChild(0).gameObject.SetActive(true);
         transform.GetChild(1).gameObject.SetActive(true);
         transform.GetChild(2).gameObject.SetActive(true);
         transform.GetChild(3).gameObject.SetActive(true);
+    }
+
+    void OnClickBackButton()
+    {
+        wallCamera.SetActive(true);
+        genCanvas.SetActive(true);
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                bandArray[i].transform.GetChild(j).gameObject.SetActive(false);
+            }
+        }
+        backButton.gameObject.SetActive(false);
     }
 
 }
--- Assets/Scripts/comeBackButton.cs
+++ Assets/Scripts/comeBackButton.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class comeBackButton : MonoBehaviour
 {
+    GameObject father;
+    public GameObject wallCamera;
+    public GameObject genCanvas;
+    public GameObject[] bandArray= new GameObject[4];
+
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
@@ -18,6 +23,18 @@ public class comeBackButton : MonoBehaviour
 
     private void OnMouseDown()
     {
-        
+        father = GameObject.Find("QuizObjects");
+        wallCamera.SetActive(true);
+        genCanvas.SetActive(true);
+
+        for (int i = 0; i<4; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                bandArray[i].transform.GetChild(j).gameObject.SetActive(false);
+            }
+        }
+        gameObject.SetActive(false);
+
     }
 }
