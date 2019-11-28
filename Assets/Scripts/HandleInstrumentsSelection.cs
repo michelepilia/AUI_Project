0b86f8fa6085641a7b452bfc5a3e295315d39ba1
Antonino96 <10546001@polimi.it>
2019-11-28 12:54:27 +0100

    .

---
+++ Assets/Scripts/HandleInstrumentsSelection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleInstrumentsSelection : MonoBehaviour
+{
+
+    public GameObject[] instruments;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DeselectAllExcept() {
+        foreach (GameObject item in instruments)  
+        {
+            item.GetComponent<InstrumentSelection>().Deselect();
+        }
+    }
+}
